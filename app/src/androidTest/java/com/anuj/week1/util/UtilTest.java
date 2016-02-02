1a0738a33b39a8b6c5ba725adfeabe5395379170
anujacharya <anujacharya1@gmail.com>
2016-02-01 19:56:29 -0800

    first

---
+++ app/src/androidTest/java/com/anuj/week1/util/UtilTest.java
@@ -0,0 +1,14 @@
+package com.anuj.week1.util;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by anujacharya on 2/1/16.
+ */
+public class UtilTest extends ApplicationTestCase<Application> {
+
+    public UtilTest(Class<Application> applicationClass) {
+        super(applicationClass);
+    }
+}
---
+++ app/src/main/java/com/anuj/util/Util.java
@@ -0,0 +1,9 @@
+package com.anuj.util;
+
+/**
+ * Created by anujacharya on 2/1/16.
+ */
+public class Util {
+
+    public static final String KEY = "e05c462ebd86446ea48a5af73769b602";
+}

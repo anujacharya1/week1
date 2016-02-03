754f353a586a076599eb5e7b66372c92b0e44655
anujacharya <anujacharya1@gmail.com>
2016-02-02 23:04:24 -0800

    basic skeleton

--- app/src/main/java/com/codepath/activities/MainActivity.java
+++ app/src/main/java/com/codepath/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.codepath.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import anuj.com.week1.R;
 
 
 public class MainActivity extends ActionBarActivity {
--- app/src/main/java/com/codepath/utils/MainUtil.java
+++ app/src/main/java/com/codepath/utils/MainUtil.java
@@ -3,7 +3,7 @@ package com.codepath.utils;
 /**
  * Created by anujacharya on 2/1/16.
  */
-public class Util {
+public class MainUtil {
 
     public static final String KEY = "e05c462ebd86446ea48a5af73769b602";
 }
